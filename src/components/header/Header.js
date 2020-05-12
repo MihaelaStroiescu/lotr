2c8ba6d2e6fc1da3e4037427569eea1e0e08b7d6
Miha Stroiescu <mihaela@stroiescu.ro>
2020-05-12 15:20:01 +0300

    fixed background image

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -29,7 +29,16 @@ function Header() {
                     <div className="nav-logo"><NavLink to="/"><img alt="Logo" src="/lotr.jpg" /></NavLink></div>
                     <div className="navigation-bar-items">
                         <ul>
-                            <li><NavLink activeClassName="active" to="/">Home  {userName}</NavLink></li>
+                            <li>{(userName || localStorage.userName ?
+                                <>
+                                    <NavLink activeClassName="active" to="/">Welcome {userName}</NavLink>
+                                </>
+                                :
+                                <>
+                                    <NavLink activeClassName="active" to="/">Home</NavLink>
+                                </>
+                            )}
+                            </li>
                             <li><NavLink activeClassName="active" to="/books">Books</NavLink></li>
                             <li><NavLink activeClassName="active" to="/movies">Movies</NavLink></li>
                             <li>{(userName  || localStorage.userName ?
